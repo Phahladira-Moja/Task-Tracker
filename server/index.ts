55561efe35faf6986a4be33b00dcf756bf4a492d
Phahladira-Moja <phahladira@patala.co.za>
2023-11-22 23:26:32 +0200

    Created the auth and tasks route without controller
    - Added rate limiter, helmet and morgan

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,45 @@
-import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import { rateLimit } from "express-rate-limit";
+import express, { Request, Response, Application } from "express";
+
+const authRouter = require("./routes/auth_routes");
+const tasksRouter = require("./routes/tasks_routes");
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Use Helmet!
+app.use(helmet());
+
+// Use Morgan!
+app.use(morgan("combined"));
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  limit: 20,
+  legacyHeaders: false,
+  message: "You have exceeded your 20 requests per 5 minute limit.",
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express & TypeScript Server");
 });
 
+app.use("/api/v1/users", authRouter);
+app.use("/api/v1/tasks", tasksRouter);
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ server/routes/tasks_routes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", () => {});
+router.post("/", () => {});
+
+router.patch("/:id", () => {});
+router.delete("/:id", () => {});
+
+module.exports = router;
