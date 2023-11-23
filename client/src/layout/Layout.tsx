0de5fd0fcb6c04850aa714843f8eab46fcb3e90e
Phahladira-Moja <phahladira@patala.co.za>
2023-11-23 22:51:48 +0200

    Added the style for background and added tailwind.config data

---
+++ client/src/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="gradient"></div>
+      </div>
+
+      <main className="app">
+        <p>Hello</p>
+      </main>
+    </>
+  );
+};
+
+export default Layout;
