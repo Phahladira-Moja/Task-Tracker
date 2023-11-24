559061ba7e4138805dee3ebb5741b2bdba11cb44
Phahladira-Moja <phahladira@patala.co.za>
2023-11-24 12:06:36 +0200

    Created the Auth Modal for login and Sign up
    - Added dropdown menu nav
    - Added a few shadcn components

---
+++ client/src/components/AuthModal.tsx
@@ -0,0 +1,129 @@
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+
+interface AuthModalProps {
+  isLogin: boolean;
+}
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(3, {
+      message: "Username must be at least 2 characters.",
+    })
+    .max(30, {
+      message: "Username must be at most 30 characters.",
+    }),
+  password: z.string().min(4, {
+    message: "Password must be at least 4 characters.",
+  }),
+});
+
+export default function AuthModal({ isLogin }: AuthModalProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      // isLogin ? await axios.post("/api/servers", values) : await axios.post("/api/servers", values);
+
+      form.reset();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" size="sm" className={cn("btn", "w-full ")}>
+          {isLogin ? "Login" : "Sign Up"}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{isLogin ? "Login" : "Sign Up"}</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Username"
+                      {...field}
+                      disabled={isLoading}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      {...field}
+                      disabled={isLoading}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button
+                disabled={isLoading}
+                type="submit"
+                className={cn("btn", "w-full")}
+              >
+                {isLogin ? "Login" : "Sign Up"}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-const Header = () => {
-  const isLoggedIn = true;
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import AuthModal from "./AuthModal";
 
-  const [toggleDropdown, setToggleDropdown] = useState(false);
+const Header = () => {
+  const isLoggedIn = false;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <div className="flex gap-2 flex-center">
@@ -19,47 +27,89 @@ const Header = () => {
       </div>
 
       <div className="sm:flex hidden">
-        {isLoggedIn ? (
-          <div className="flex gap-3 md:gap-4 rounded">
+        <div className="flex-center gap-3 md:gap-4 rounded">
+          {isLoggedIn && (
             <Button variant="default" size="sm">
               Create Task
             </Button>
-            <Button variant="outline" size="sm">
-              Logout
-            </Button>
-          </div>
-        ) : (
-          <Button variant="default" size="sm">
-            Login
-          </Button>
-        )}
+          )}
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="hover:cursor-pointer">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="mr-16">
+              {isLoggedIn ? (
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  className={cn("btn", "w-full")}
+                >
+                  Logout
+                </Button>
+              ) : (
+                <>
+                  <AuthModal isLogin />
+                  <DropdownMenuSeparator />
+                  <AuthModal isLogin={false} />
+                </>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isLoggedIn ? (
-          <div className="flex">
-            <Avatar onClick={() => setToggleDropdown((prev) => !prev)}>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="hover:cursor-pointer">
               <AvatarImage src="https://github.com/shadcn.png" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-
-            {toggleDropdown && (
-              <div className="dropdown">
-                <Button variant="default" size="sm">
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-8 w-5">
+            {isLoggedIn ? (
+              <>
+                <Button
+                  variant="default"
+                  size="sm"
+                  className={cn("btn", "w-full mb-2")}
+                >
                   Create Task
                 </Button>
-                <Button variant="outline" size="sm">
-                  Sign Out
+                <Button
+                  variant="destructive"
+                  size="sm"
+                  className={cn("btn", "w-full")}
+                >
+                  Logout
                 </Button>
-              </div>
+              </>
+            ) : (
+              <>
+                <Button
+                  variant="default"
+                  size="sm"
+                  className={cn("btn", "w-full mb-2")}
+                >
+                  Login
+                </Button>
+                <DropdownMenuSeparator />
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className={cn("btn", "w-full")}
+                >
+                  Sign up
+                </Button>
+              </>
             )}
-          </div>
-        ) : (
-          <Button variant="default" size="sm">
-            Sign In
-          </Button>
-        )}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
